71b67d4aed50478a311a601004faf6026d986a9a
Lakshy Aggarwal <lakshyaggarwal18sep@gmail.com>
2026-06-18 22:58:13 +0530

    added all features

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage({
   destination: "uploads",
   filename: (req, file, cb) => {
     return cb(null, `${Date.now()}${file.originalname}`);
